520a900cf8197aff0814eee1ea437833716c2bd5
Robbie Nawrocki <48452340+rnawrock@users.noreply.github.com>
2019-04-28 20:11:31 -0400

    Update rideshare_time.R

--- rideshare/rideshare_time.R
+++ rideshare/rideshare_time.R
@@ -26,4 +26,9 @@ uber$time <- hour(uber$hour)
 daily_pattern <- uber %>% group_by(uber$time) %>% summarize(n()/(nrow(daily_rides)))
 colnames(daily_pattern) <- c('hour', 'mean_ridership')
 
-ggplot(data = daily_pattern, aes(x = hour, y = mean_ridership)) + geom_line(linetype = 'solid', col = 'springgreen3') + geom_point(col = 'springgreen4') + labs(x = 'Hour of the day', y = 'Mean Ridership', title = 'Uber Daily Ridership Pattern')
+ggplot(data = daily_pattern, aes(x = hour, y = mean_ridership)) + 
+  geom_area(linetype = 'solid', fill = 'springgreen3') + 
+  geom_point(col = 'springgreen4') + 
+  labs(x = 'Hour of the Day', 
+       y = 'Mean Ridership', 
+       title = 'Jan.-June 2015 Uber Daily Ridership Pattern by Hour')
